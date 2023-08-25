25ef4ff66be8e1f811b0169d34316ba6df341bc6
Rahul Prasad <rpsp981@gmail.com>
2023-08-25 20:52:21 +0530

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 216, 128, 50)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Rahuls App'),
+    );
+  }
+}
+
+class Greet extends StatefulWidget {
+  const Greet({super.key});
+
+  @override
+  State<Greet> createState() => _GreetState();
+}
+
+class _GreetState extends State<Greet> {
+  var name = "";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [Text("Bonjour $name",style: const TextStyle(fontSize: 20)), const Text("!!!")],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 24, left: 24),
+          child: TextField(
+              onChanged: (value) => setState(() {
+                    name = value;
+                  })),
+        )
+      ],
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body:
+          Greet(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
